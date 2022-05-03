2c7e69e37e5b32243e7f4c7b5455a170c23627f4
Wilton Ramos <wrs@cin.ufpe.br>
2022-05-02 21:59:54 -0300

    Ajusta Responses que precisam de Track

--- digai/digai/Networking/Responses/FetchRoomsResponse.swift
+++ digai/digai/Networking/Responses/FetchRoomsResponse.swift
@@ -8,28 +8,8 @@
 public struct FetchRoomsResponse: Codable {
     let id: String
     let players: [String]
-    let tracks: [AudioTrack]
+    let tracks: [Track]
     let started: Bool
     let steps: Int
     let genres: [String]
-}
-
-extension FetchRoomsResponse {
-    struct Track: Codable {
-        let name: String
-        let artist: String
-        let album: String
-        let albumArt: String
-        let preview: String
-        let uri: String
-        
-        enum CodingKeys: String, CodingKey {
-            case name
-            case artist
-            case album
-            case albumArt = "album_art"
-            case preview = "preview_url"
-            case uri
-        }
-    }
 }
